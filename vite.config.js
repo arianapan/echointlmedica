b78d7a2a7ef98a3616acd9f2974bbae01ce2b35b
larrysimingdeng <siming_deng_stat@163.com>
2025-08-23 23:14:19 +0800

    Restore correct vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/echointlmedica/',  // 设置为仓库名，除非你有自定义域名
+  base: '/',  // 因为使用自定义域名，所以设置为根路径
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
