9121e59e2f43a1faf1ff9e787544360bbb8ff9c6
larrysimingdeng <siming_deng_stat@163.com>
2025-08-23 15:18:33 +0800

    github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // 因为使用自定义域名，所以设置为根路径
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
